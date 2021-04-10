75e41783f1decf5844b89f6b800663bfef7bb566
iamayushdas <ayush24das@gmail.com>
2021-04-10 19:42:29 +0530

    lighthouse mark increased

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -17,10 +17,28 @@ self.addEventListener('install', (event) => {
 
 // listen for requests
 self.addEventListener('fetch', (event) => {
-
+    event.respondWith(
+        caches.match(event.request)
+        .then(() => {
+            return fetch(event.request)
+            .catch(() => caches.match('offline.html'))
+        })
+    )
 });
 
 // activate the sw
 self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
 
+    event.waitUntil(
+        caches.keys()
+        .then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
 });
